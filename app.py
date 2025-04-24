d445b4b0190bab5c23817760d605303374a9b1bd
Najla Dhia <109951025+najlalala@users.noreply.github.com>
2025-04-24 23:36:42 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
 import pickle
 import pandas as pd
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-def preprocessing(data, ohe):  
+def preprocessing(data, ohe):
+    
     data["Gender"] = data["Gender"].replace({
     "Male": 0,
     "Female": 1
     })
     
-    data_ohe = pd.DataFrame(ohe.transform(data[["Occupation", "BMI Category"]]))
-    data.drop(["Occupation", "BMI Category"], axis=1, inplace=True)
+    data_ohe = pd.DataFrame(ohe.transform(data[["BMI Category"]]))
+    data.drop(["BMI Category"], axis=1, inplace=True)
     data = pd.concat([data, data_ohe], axis=1)
-    
-    data["systolic"] = data["Blood Pressure"].apply(lambda x: int(x.split("/")[0]))
-    data["diastolic"] = data["Blood Pressure"].apply(lambda x: int(x.split("/")[1]))
-
-    data.drop(["Blood Pressure"], axis=1, inplace=True)
     
     return data
 
@@ -38,75 +34,53 @@ def load_ohe():
     return ohe
 
 @app.route('/')
-def index():
-    return 'Hello World'
+def home():
+    return render_template('home.html')
 
-@app.route('/predict', methods=['GET'])
-def predict():
-    ohe = load_ohe()
-    model = load_model()
-    
-    gender = request.args.get("gender")
-    age = request.args.get("age")
-    occupation = request.args.get("occupation")
-    sleep_duration = request.args.get("sleep_duration")
-    quality_of_sleep = request.args.get("quality_of_sleep")
-    physical_activity = request.args.get("physical_activity")
-    stress_level = request.args.get("stress_level")
-    bmi_category = request.args.get("bmi_category")
-    blood_pressure = request.args.get("blood_pressure")
-    heart_rate = request.args.get("heart_rate")
-    daily_steps = request.args.get("daily_steps")
+@app.route('/input')
+def input():
+    return render_template('input.html')
 
-    data = pd.DataFrame({
-        "Gender": [gender],
-        "Age": [age],
-        "Occupation": [occupation],
-        "Sleep Duration": [sleep_duration],
-        "Quality of Sleep": [quality_of_sleep],
-        "Physical Activity Level": [physical_activity],
-        "Stress Level": [stress_level],
-        "BMI Category": [bmi_category],
-        "Blood Pressure": [blood_pressure],
-        "Heart Rate": [heart_rate],
-        "Daily Steps": [daily_steps]
-    })
-    
-    processed_data = preprocessing(data, ohe)
-    processed_data.columns = processed_data.columns.astype(str)
-    prediction = model.predict(processed_data)
-    hasil = index_to_label(prediction[0])
-    
-    return hasil 
-
-@app.route('/predict1', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     ohe = load_ohe()
     model = load_model()
     
     # Extracting data from the JSON request
+    nama = request.form.get("name").capitalize()
     gender = request.form.get("gender")
     age = request.form.get("age")
-    occupation = request.form.get("occupation")
+    weight = float(request.form.get("weight"))
+    height = float(request.form.get("height")) 
     sleep_duration = request.form.get("sleep_duration")
-    quality_of_sleep = request.form.get("quality_of_sleep")
-    physical_activity = request.form.get("physical_activity")
-    stress_level = request.form.get("stress_level")
-    bmi_category = request.form.get("bmi_category")
-    blood_pressure = request.form.get("blood_pressure")
+    activity = float(request.form.get("activity"))
     heart_rate = request.form.get("heart_rate")
     daily_steps = request.form.get("daily_steps")
 
+    # Hitung BMI
+    height /= 100 
+    bmi = weight / (height * height)
+
+    # Kategori BMI
+    if bmi < 18.5:
+        bmi_category = "Underweight"
+    elif 18.5 <= bmi < 24.9:
+        bmi_category = "Normal Weight"
+    elif 25 <= bmi < 29.9:
+        bmi_category = "Overweight"
+    else:
+        bmi_category = "Obese"
+
+    # Ubah ke Menit
+    physical_activity = activity * 60
+
+    # Creating a DataFrame from the JSON data
     data = pd.DataFrame({
         "Gender": [gender],
         "Age": [age],
-        "Occupation": [occupation],
         "Sleep Duration": [sleep_duration],
-        "Quality of Sleep": [quality_of_sleep],
         "Physical Activity Level": [physical_activity],
-        "Stress Level": [stress_level],
         "BMI Category": [bmi_category],
-        "Blood Pressure": [blood_pressure],
         "Heart Rate": [heart_rate],
         "Daily Steps": [daily_steps]
     })
@@ -115,8 +89,40 @@ def predict():
     processed_data.columns = processed_data.columns.astype(str)
     prediction = model.predict(processed_data)
     result = index_to_label(prediction[0])
-    
-    return str(result)
+    result = str(result)
+
+    if result == 'Normal' :
+        return render_template('normal.html', result=result, nama=nama)
+    elif result == 'Insomnia' :
+        return render_template('insomnia1.html', result=result, nama=nama)
+    else:
+        return render_template('sleepapnea.html', result=result, nama=nama)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/information')
+def information():
+    return render_template('information.html')
+
+@app.route('/sleep_apnea')
+def sleep_apnea():
+    return render_template('sleep_apnea.html')
+
+@app.route('/sleep_disorder')
+def sleep_disorder():
+    return render_template('sleep_disorder.html')
+
+@app.route('/insomnia')
+def insomnia():
+    return render_template('insomnia.html')
+
+@app.route('/ourteam')
+def ourteam():
+    return render_template('ourteam.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
